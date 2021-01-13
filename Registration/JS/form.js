71607f3a0f1fff57876c89dd6b454857c1f72d66
Sonali3010 <sonalikushwaha30@gmail.com>
2021-01-13 17:28:07 +0530

    delete button Changes added

--- Registration/JS/form.js
+++ Registration/JS/form.js
@@ -1,12 +1,6 @@
 const submitButton = document.getElementById("button");
 const listOfPeople = [];
 let stringifiedDetailsOfPeople, detailsOfPeople;
-const url = "https://crudcrud.com/api/b01f16c0351542b98abb82bc7011a88f";
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Inside DOM..");
-  localStorage.getItem(stringifiedDetailsOfPeople);
-});
 
 document.addEventListener("click", () => {
   const name = document.getElementById("name").value;
@@ -24,7 +18,7 @@ document.addEventListener("click", () => {
       time: time
     };
     localStorage.setItem("userDetails" + emailId, JSON.stringify(object)); //for setting in local storage
-    console.log( JSON.stringify(object));
+    //console.log( JSON.stringify(object));
     listOfPeople.push(object);
   
   
@@ -38,14 +32,14 @@ document.addEventListener("click", () => {
       //we only care about keys that start with userDetails
       //this is called regex matching
       stringifiedDetailsOfPeople = localStorage.getItem(keys[i]);
-      console.log("stringifiedDetailsOfPeople", stringifiedDetailsOfPeople);
+    //  console.log("stringifiedDetailsOfPeople", stringifiedDetailsOfPeople);
       detailsOfPeople = JSON.parse(stringifiedDetailsOfPeople);
-      console.log("details", detailsOfPeople);
+      //console.log("details", detailsOfPeople);
 
-      addNewLineElement(detailsOfPeople);
+     addNewLineElement(detailsOfPeople);
     }
   }
-  //  addNewLineElement(object);
+ //addNewLineElement(object);
   }
 });
 
@@ -68,5 +62,33 @@ function addNewLineElement(object) {
     )
   );
   ul.appendChild(li);
- 
+  const a1 = document.createElement("input");
+  a1.id = "b1";
+  a1.type = "button";
+  a1.value = "Edit";
+  a1.addEventListener("click", () => {
+    console.log(object.name);
+    localStorage.setItem("name",li.getElementById("name"));
+    localStorage.setItem("email",li.getElementById("email"));
+  });
+  a1.className = "edit";
+  a1.style.border = "2px solid green";
+  console.log(a1);
+  li.appendChild(a1);
+
+  const a2 = document.createElement("input");
+  a2.type = "button";
+  a2.value = "delete";
+  a2.addEventListener("click", () => {
+    li.remove();
+  });
+  a2.className = "delete";
+  a2.style.border = "2px solid red";
+  console.log(a2);
+  li.appendChild(a2);
+  console.log(li);
+
+  ul.appendChild(li);
+
+
   }
